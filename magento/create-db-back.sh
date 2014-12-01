bc89af131d83a69d11e96fe34d39298235b17307
Brent W Peterson <brent@wagento.com>
2014-12-01 22:44:22 +0000

    Update path statements

--- magento/create-db-back.sh
+++ magento/create-db-back.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "Enter path for local.xml, followed by [ENTER]:"
-read path
+echo "Enter path for local.xml (Only enter the application path, not the trailing /), followed by [ENTER]:"
+read appath
+path="$appath/app/etc/local.xml"
 if [ -e "$path" ]
 then
 echo "Name of file (letsdump.sh is default, followed by [ENTER]:"
@@ -12,23 +13,21 @@ password=($(cat $path | grep -oP '(?<=<password><!\[CDATA\[)[^\]]+'))
 user=($(cat $path | grep -oP '(?<=<username><!\[CDATA\[)[^\]]+'))
 prefix=($(cat $path | grep -oP '(?<=<table_prefix><!\[CDATA\[)[^\]]+'))
 echo $path
-mkdir -p ~/mysql_backup/update
-checkfile="~/mysql_backup/$file"
-echo $checkfile
-if [ -e "$checkfile" ]
-then
-mv ~/mysql_backup/$file ~/mysql_backup/$file-bak-$(date +%d%m%Y_%H%M).bak
-else
-#TODO I cant get this to work?
-echo 'file doesnt exist'
-mv ~/mysql_backup/$file ~/mysql_backup/$file-bak-$(date +%d%m%Y_%H%M).bak
+if [ ! -d "$HOME/mysql_backup/update" ]; then
+mkdir -p $HOME/mysql_backup/update
 fi
-touch ~/mysql_backup/$file
-touch ~/mysql_backup/update/clean-log.sql
-echo "MAGEFILE=\"$dbs-\$(date +%d%m%Y_%H%M).sql.gz\"" >> ~/mysql_backup/$file
-echo "mysql -p'$password' -u $user -h $host $dbs < update/clean-log.sql" >> ~/mysql_backup/$file
-echo "mysqldump -p'$password' -u$user -h$host --single-transaction --quick $dbs | gzip > \$MAGEFILE" >> ~/mysql_backup/$file
-echo "truncate $prefix dataflow_batch_export;truncate $prefix dataflow_batch_import;truncate $prefix log_customer;truncate $prefix log_quote;truncate $prefix log_summary;truncate $prefix log_summary_type;truncate $prefix log_url;truncate $prefix log_url_info;truncate $prefix log_visitor;truncate $prefix log_visitor_info;truncate $prefix log_visitor_online;truncate $prefix report_viewed_product_index;truncate $prefix report_compared_product_index;truncate $prefix report_event;truncate $prefix sendfriend_log;" > ~/mysql_backup/update/clean-log.sql
+checkfile="$HOME/mysql_backup/$file"
+if [ -e "$checkfile" ]; then
+mv $HOME/mysql_backup/$file $HOME/mysql_backup/$file-bak-$(date +%d%m%Y_%H%M).bak
 else
- echo 'You really screwed up on the path, dude try this again'
+echo 'file doesnt exist'
+fi
+touch $HOME/mysql_backup/$file
+touch $HOME/mysql_backup/update/clean-log.sql
+echo "MAGEFILE=\"$dbs-\$(date +%d%m%Y_%H%M).sql.gz\"" >> $HOME/mysql_backup/$file
+echo "mysql -p'$password' -u $user -h $host $dbs < update/clean-log.sql" >> $HOME/mysql_backup/$file
+echo "mysqldump -p'$password' -u$user -h$host --single-transaction --quick $dbs | gzip > \$MAGEFILE" >> $HOME/mysql_backup/$file
+echo "truncate $prefix dataflow_batch_export;truncate $prefix dataflow_batch_import;truncate $prefix log_customer;truncate $prefix log_quote;truncate $prefix log_summary;truncate $prefix log_summary_type;truncate $prefix log_url;truncate $prefix log_url_info;truncate $prefix log_visitor;truncate $prefix log_visitor_info;truncate $prefix log_visitor_online;truncate $prefix report_viewed_product_index;truncate $prefix report_compared_product_index;truncate $prefix report_event;truncate $prefix sendfriend_log;" > $HOME/mysql_backup/update/clean-log.sql
+else
+ echo "You really screwed up on the path, dude try this again. Did you screw up on the path? -->  $path"
 fi

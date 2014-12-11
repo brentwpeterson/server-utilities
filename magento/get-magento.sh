8b328a5977027ac279cb401896db75ada1dfc8af
Brent W. Peterson <creativedata@gmail.com>
2014-12-11 07:32:49 -0600

    Update get-magento.sh
    Add future option to get Vinai's sample data when getting a new version of Magento

--- magento/get-magento.sh
+++ magento/get-magento.sh
@@ -1,4 +1,8 @@
 echo "Enter Magento Version to download, followed by [ENTER]:"
 read version
 wget http://www.magentocommerce.com/downloads/assets/$version/magento-$version.tar.gz
+#####
+# TODO : Add this as an option for Sample data
+#####
+## git clone https://github.com/Vinai/compressed-magento-sample-data.git
 

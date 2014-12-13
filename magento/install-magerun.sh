bf583550dda7b4de5eae7e1c853796f60da25907
Wagento Support <support@wagento.com>
2014-12-13 18:18:09 +0000

    add Magerun script

---
+++ magento/install-magerun.sh
@@ -0,0 +1,5 @@
+cd ~/
+wget https://raw.githubusercontent.com/netz98/n98-magerun/master/n98-magerun.phar
+chmod +x ./n98-magerun.phar
+sudo cp ./n98-magerun.phar /usr/local/bin/
+sed -i '2ialias magerun="~/./n98-magerun.phar"' ~/.bashrc

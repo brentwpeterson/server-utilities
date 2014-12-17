366cd8a221c61e6750f5ab5849b668f583c80ff3
Wagento Support <support@wagento.com>
2014-12-17 16:45:24 -0500

    Add version default and download location to get Magento

--- magento/get-magento.sh
+++ magento/get-magento.sh
@@ -1,6 +1,10 @@
-echo "Enter Magento Version to download, followed by [ENTER]:"
+echo "Enter Magento Version to download, (Default is 1.9.1.0)followed by [ENTER]:"
 read version
-wget http://www.magentocommerce.com/downloads/assets/$version/magento-$version.tar.gz
+echo "Do you want to download from a different locaion? The default is ~/ followed by [ENTER]:"
+read backdir
+backdir=${backdir:-$HOME}
+version=${version:-1.9.1.0}
+wget http://www.magentocommerce.com/downloads/assets/$version/magento-$version.tar.gz -P $backdir
 #####
 # TODO : Add this as an option for Sample data
 #####

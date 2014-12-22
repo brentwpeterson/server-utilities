28d2d378a175b7f3f00f57c1318b22b8ff5b7f51
Brent W. Peterson <creativedata@gmail.com>
2014-12-21 18:04:23 -1000

    Update find-large-file.sh

--- utilities/find-large-file.sh
+++ utilities/find-large-file.sh
@@ -1,5 +1,5 @@
-#######
-# The is a work in progress: #TODO add size as an interactive option
+###############
+# The is a work in progress: #TODO add size and path as an interactive option
 ################
 
 find / -type f -size +500M -exec ls -lh {} \;

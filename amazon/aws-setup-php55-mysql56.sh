1e54a93213a6c0d602a710d6ebc5593dd767279d
Brent Peterson <brent@wagento.com>
2014-12-25 09:39:17 -1000

    Start on new Percona install

---
+++ amazon/aws-setup-php55-mysql56.sh
@@ -0,0 +1,32 @@
+#########
+# This will currently install PHP5.4 and MySQL 5.5 I am working on a new version to install MySQL 5.6
+# TODO http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm
+#########
+yum groupinstall 'Development Tools'
+yum install percona-server-server-5.6 percona-server-client-5.6
+yum -y install ntp httpd24 mod24_ssl 
+#mysql-server php55 php55-mysql php55-mbstring
+chkconfig --levels 235 mysqld on
+/etc/init.d/mysqld start
+chkconfig --levels 235 httpd on
+/etc/init.d/httpd start
+#yum -y install php54 php54-devel php54-gd php54-imap php54-ldap php54-mysql php54-odbc php54-pear php54-xml php54-xmlrpc php54-pecl-apc php54-mbstring php54-mcrypt php54-mssql php54-snmp php54-soap php54-tidy curl curl-devel perl-libwww-perl ImageMagick libxml2 libxml2-devel mod24_fcgid php54-cli httpd24-devel mutt
+#mv /etc/httpd/conf.d/fcgid.conf /etc/httpd/conf.d/fcgid.bak
+#mkdir -p /etc/httpd/vhosts.d
+#mkdir -p /var/www/vhosts/
+#sed -i '95iServerName localhost' /etc/httpd/conf/httpd.conf
+#echo 'IncludeOptional vhosts.d/*.conf' >> /etc/httpd/conf/httpd.conf
+#hostname
+#read hostn
+#ip='127.0.0.1 '
+#addhost = ${ip}${hostn}
+#echo $addhost >> /etc/hosts
+#service httpd restart; service mysqld restart
+#yum update
+#service httpd restart; service mysqld restart
+#cd /home/ec2-user/
+#wget https://raw.githubusercontent.com/netz98/n98-magerun/master/n98-magerun.phar
+#chmod +x ./n98-magerun.phar
+#sudo cp ./n98-magerun.phar /usr/local/bin/
+#sed -i '2ialias magerun="~/./n98-magerun.phar"' /home/ec2-user/.bashrc
+

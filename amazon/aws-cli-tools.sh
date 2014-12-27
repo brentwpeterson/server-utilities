a0d89be8be70a0f0f35f41787f7477677969a4b2
EC2 Default User <ec2-user@ip-172-31-61-68.ec2.internal>
2014-12-27 15:51:46 +0000

    update aws shortcuts

---
+++ amazon/aws-cli-tools.sh
@@ -0,0 +1,10 @@
+read -p "This will add your Amazon keys to your bashrc, are you sure?  [yn]" answer
+if [[ $answer = y ]] ; then
+echo "Enter your Amazon Access key [ENTER]:"
+read awskey
+echo "Enter your Amazon TOP secret key [ENTER]:"
+read awssecret
+echo "export AWS_ACCESS_KEY=$awskey" >> $HOME/.bashrc
+echo "export AWS_SECRET_KEY=$awssecret" >> $HOME/.bashrc
+source $HOME/.bashrc
+fi

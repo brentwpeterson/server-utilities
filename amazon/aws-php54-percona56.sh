d76726cf7336a2a3a1e10dc6231951c968b026fb
Brent Peterson <brent@wagento.com>
2014-12-28 23:01:02 +0000

    Add new files

--- amazon/aws-php54-percona56.sh
+++ amazon/aws-php54-percona56.sh
@@ -11,26 +11,36 @@ chkconfig --levels 235 mysqld on
 chkconfig --levels 235 httpd on
 /etc/init.d/httpd start
 yum -y install php54 php54-devel php54-gd php54-imap php54-ldap php54-mysql php54-odbc php54-pear php54-xml php54-xmlrpc php54-pecl-apc php54-mbstring php54-mcrypt php54-snmp php54-soap php54-tidy curl curl-devel perl-libwww-perl ImageMagick libxml2 libxml2-devel mod24_fcgid php54-cli httpd24-devel mutt
-#mv /etc/httpd/conf.d/fcgid.conf /etc/httpd/conf.d/fcgid.bak
-#mkdir -p /etc/httpd/vhosts.d
-#mkdir -p /var/www/vhosts/
-#sed -i '95iServerName localhost' /etc/httpd/conf/httpd.conf
-#echo 'IncludeOptional vhosts.d/*.conf' >> /etc/httpd/conf/httpd.conf
+mv /etc/httpd/conf.d/fcgid.conf /etc/httpd/conf.d/fcgid.bak
+mkdir -p /etc/httpd/vhosts.d
+mkdir -p /var/www/vhosts/
+sed -i '95iServerName localhost' /etc/httpd/conf/httpd.conf
+echo 'IncludeOptional vhosts.d/*.conf' >> /etc/httpd/conf/httpd.conf
 #hostname
 #read hostn
 #ip='127.0.0.1 '
 #addhost = ${ip}${hostn}
 #echo $addhost >> /etc/hosts
-#service httpd restart; service mysqld restart
-#yum update
-#service httpd restart; service mysqld restart
-#cd /home/ec2-user/
-#wget https://raw.githubusercontent.com/netz98/n98-magerun/master/n98-magerun.phar
-#chmod +x ./n98-magerun.phar
-#sudo cp ./n98-magerun.phar /usr/local/bin/
-#sed -i '2ialias magerun="~/./n98-magerun.phar"' /home/ec2-user/.bashrc
+service httpd restart; service mysqld restart
+yum update
+service httpd restart; service mysqld restart
+cd /home/ec2-user/
+wget https://raw.githubusercontent.com/netz98/n98-magerun/master/n98-magerun.phar
+chmod +x ./n98-magerun.phar
+sudo cp ./n98-magerun.phar /usr/local/bin/
+sed -i '2ialias magerun="~/./n98-magerun.phar"' /home/ec2-user/.bashrc
 ###################
 # http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/install-LAMP.html
 # TODO need to add/update the amazon permissions
-###################
-
+################### 
+echo "Do you want add a new user to your www directory Leave blank for ec2-user? [ENTER]:"
+read newuser
+newuser=${newuser:-ec2-user}
+groupadd www
+usermod -a -G www $newuser
+chown -R root:www /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} +
+find /var/www -type f -exec sudo chmod 0664 {} +
+sed -i 's/User apache/User $newuser/g' /etc/httpd/conf/httpd.conf
+sed -i 's/Group apache/Group www/g' /etc/httpd/conf/httpd.conf
---
+++ amazon/create-http.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# This script will install all required data for a development environment.
+echo "Enter the username, followed by [ENTER]:"
+read user
+echo "Enter the password for the account created, followed by [ENTER]:"
+read password
+echo "Adding user"
+useradd $user
+echo $password | passwd $user --stdin
+
+##
+# Create required directories
+##
+echo "Creating Needed Directories"
+cd /home/$user
+chmod 755 /home/$user
+mkdir -p /var/www/vhosts/$user/html
+mkdir -p /var/www/vhosts/$user/logs
+mkdir -p /var/www/vhosts/$user/mysql_backup
+chown -R $user:$user /var/www/vhosts/$user/
+#
+# Create apache conf
+##
+echo "Creating Apache Configuration"
+sed -e "s;%USER%;$user;" /home/ec2-user/server-scripts/amazon/base.conf > '/etc/httpd/vhosts.d/'$user'.conf'
+echo "Restarting Apache Gracefully"
+service httpd graceful
+
+##
+# create SSH keys
+##
+echo "Creating RSA Keys"
+mkdir /home/$user/.ssh
+chmod 777 /home/$user/.ssh
+su $user -c "ssh-keygen -t rsa -f /home/$user/.ssh/id_rsa"
+chmod 700 /home/$user/.ssh
+chown -R $user:$user /home/$user
+##
+# Send email with id.rsa.pub attached
+##
+echo "This is the message body" | mutt -s "Development Information" -a "/home/$user/.ssh/id_rsa.pub" -- brent@wagento.com
+##
+# return to root directory
+##
+cd ~
+echo "Copy and paste key to codebase"
+su $user -c "cat ~/.ssh/id_rsa.pub"
+read -p "Do you want to setup a repo now?" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+echo "Enter GIT Repo adddress, followed by [ENTER]:"
+read gitrepo
+cd /var/www/vhosts/$user/html
+su $user -c "git clone $gitrepo ."
+su $user -c "mkdir var"
+su $user -c "mkdir media"
+su $user -c "chmod -R o+w media var app/etc"
+fi

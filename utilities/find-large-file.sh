2f6a33a5651506f83d16865f634201a7862e3cc3
Brent Peterson <brent@wagento.com>
2014-12-29 08:42:31 -0600

    change to top 10 files

--- utilities/find-large-file.sh
+++ utilities/find-large-file.sh
@@ -1,5 +1,5 @@
 ###############
 # The is a work in progress: #TODO add size and path as an interactive option
 ################
-
-find / -type f -size +500M -exec ls -lh {} \;
+find / -type f -printf '%s %p\n'| sort -nr | head -10
+#find / -type f -size +500M -exec ls -lh {} \;

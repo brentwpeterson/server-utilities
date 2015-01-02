4d190c34cc8e5d5f61452e08a3f55134c40f04a7
Brent Peterson <brent@wagento.com>
2015-01-02 19:53:15 +0000

    Add auto password to database script

--- amazon/database-setup.sh
+++ amazon/database-setup.sh
@@ -6,10 +6,10 @@ function createDatabase ()
     
     MYSQL=`which mysql`
 
-    Q1="CREATE DATABASE IF NOT EXISTS $1;"
+    Q1="CREATE DATABASE IF NOT EXISTS $3;"
     Q2="CREATE USER '$1'@'localhost' IDENTIFIED BY '$2';"
-    Q3="GRANT USAGE on *.* to '$1'@'localhost' IDENTIFIED BY '$2';"
-    Q4="GRANT ALL PRIVILEGES ON $1.* TO '$1'@'localhost' IDENTIFIED BY '$2';"
+    Q3="GRANT USAGE on $3.* to '$1'@'localhost' IDENTIFIED BY '$2';"
+    Q4="GRANT ALL PRIVILEGES ON $3.* TO '$1'@'localhost' IDENTIFIED BY '$2';"
     Q5="FLUSH PRIVILEGES;"
     SQL="${Q1}${Q2}${Q3}${Q4}${Q5}"
     
@@ -23,14 +23,22 @@ function createDatabase ()
 ##
 # Create username and set password
 ##
-echo "Enter the username, followed by [ENTER]:"
+echo "Enter the username [ENTER]:"
 read user
-echo "Enter the password for the account created, followed by [ENTER]:"
-read password
-echo "Adding user"
+echo "Enter the database name [ENTER]:"
+read db
 
+password=$(date +%s | sha256sum | base64 | head -c 10)
 ##
 # Creating Database
 ###
 echo "Starting Database creation and DB user"
-createDatabase $user $password
+createDatabase $user $password $db
+
+echo 'Did you copy the database info somewhere? ' 
+echo 'Username: '$user
+echo 'Database: '$db
+echo 'Password: '$password
+
+echo 'Test Login'
+echo "mysql -u"$user "-p'"$password"'" $db

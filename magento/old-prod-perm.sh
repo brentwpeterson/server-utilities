6252c44b11639b0e555f58767eeda81d6954c00f
Wagento Support <support@wagento.com>
2015-01-02 22:21:18 +0000

    Add my old perm script

---
+++ magento/old-prod-perm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#########################
+# Ask for the app location
+# this is from http://www.magentocommerce.com/knowledge-base/entry/install-privs-after
+#########################
+##
+# Take first positional parameter as Magento base directory
+path="$1"
+
+##
+# Validate the path
+while true; do
+    if [[ -d "$path" && -f "$path/app/Mage.php" ]]; then
+        break
+    elif [[ "$path" ]]; then
+        printf '"%s" does not appear to be a valid Magento base directory.\n' "$path"
+    fi
+    read -p 'Enter the application path, not the trailing /), followed by [ENTER]: ' path
+done
+
+##
+# Clear the umask so the below permissions are absolute.
+umask 0
+
+
+find $path/. -type f -exec chmod 400 {} \;
+find $path/. -type d -exec chmod 500 {} \; 
+find $path/var/ -type f -exec chmod 600 {} \; 
+find $path/media/ -type f -exec chmod 600 {} \;
+find $path/var/ -type d -exec chmod 700 {} \; 
+find $path/media/ -type d -exec chmod 700 {} \;
+chmod 700 $path/includes
+chmod 600 $path/includes/config.php
+find $path/.git/ -type d -exec chmod 700 {} \; 
+find $path/.git/ -type f -exec chmod 600 {} \; 
+chmod 600 $path/.gitignore 

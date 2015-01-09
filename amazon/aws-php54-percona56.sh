52d10ca124fb8fd179c97ec25d450524873777b8
Brent W. Peterson <creativedata@gmail.com>
2015-01-09 10:34:16 -0600

    Update aws-php54-percona56.sh

--- amazon/aws-php54-percona56.sh
+++ amazon/aws-php54-percona56.sh
@@ -38,7 +38,7 @@ read newuser
 newuser=${newuser:-ec2-user}
 groupadd www
 usermod -a -G www $newuser
-chown -R root:www /var/www
+chown -R $newuser:www /var/www
 chmod 2775 /var/www
 find /var/www -type d -exec sudo chmod 2775 {} +
 find /var/www -type f -exec sudo chmod 0664 {} +

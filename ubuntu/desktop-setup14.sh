13796c073a8ab9a33480ab8ce131a1de5b51b847
Brent Peterson <Brent@wagento.com>
2015-01-30 20:52:34 -0600

    Update Mysql 5.6

--- ubuntu/desktop-setup14.sh
+++ ubuntu/desktop-setup14.sh
@@ -1,5 +1,5 @@
 apt-get install ssh openssh-server vim-nox vim ntp ntpdate
-apt-get install mysql-client mysql-server openssl binutils sudo
+apt-get install mysql-client-5.6 mysql-server-5.6 openssl binutils sudo
 apt-get install apache2 
 apt-get install apache2.2-common apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert libapache2-mod-php5 php5 php5-common php5-gd php5-mysql php5-imap php5-cgi libapache2-mod-fcgid apache2-suexec php-pear php-auth php5-curl php5-mcrypt mcrypt php5-imagick imagemagick libapache2-mod-suphp libruby libapache2-mod-ruby libapache2-mod-python libapache2-mod-perl2
 a2enmod rewrite ssl actions include
@@ -11,3 +11,4 @@ a2enmod actions fastcgi alias
 /etc/init.d/apache2 restart
 sudo apt-get install default-jre
 sudo apt-get install default-jdk
+apt-get update

f11f8fbf0c7d35a4e7a465f5cf54d9a78d4f7006
Jason Barrett <jason.barrett@palmettostatearmory.com>
2015-02-01 12:36:45 -0600

    Update local files

--- magento/misc/core_check.sh
+++ magento/misc/core_check.sh
@@ -1,10 +1,12 @@
 # This is intended to check the core version of Magento with your version of Magento
 # You should download the core version of Magento and setup a GIT repo
 # the opy your core back into it to ensure you have your patches, but at this point you should check to make sure there are not files that are no pathch files
+EMAILTO="brent@wagento.com,creativedata@gmail.com"
 diff $HOME/magento-core-files/app/code/core/ $HOME/current/app/code/core/
 if [ $? -ne 0 ]
 then
-    echo "The directory was modified"
+      diff -wrql $HOME/magento-core-files/app/code/core/ $HOME/current/app/code/core/ >> $HOME/core-diff.log
+      diff -wrql $HOME/magento-core-files/app/code/core/ $HOME/current/app/code/core/ | mutt -s "Core File Changes" -- $EMAILTO
 else
     echo "Core files are the same"
 fi

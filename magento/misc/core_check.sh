230c3295041d6a6357123daf214b468cf66dc617
Jason Barrett <jason.barrett@palmettostatearmory.com>
2015-02-01 16:03:45 -0600

    Update paths

--- magento/misc/core_check.sh
+++ magento/misc/core_check.sh
@@ -6,26 +6,26 @@
 # You should download the core version of Magento and setup a GIT repo
 # the opy your core back into it to ensure you have your patches, but at this point you should check to make sure there are not files that are no pathch files
 EMAILTO="brent@wagento.com,creativedata@gmail.com"
-TOPATH=$HOME"current/"
-FROMPATH=$HOME"magento-core-files/"
+TOPATH=$HOME"/current/"
+FROMPATH=$HOME"/magento-core-files/"
 # Change the path for the two directories to diff below
 # TODO create installer that will generate a file automatically
 # TODO download CE version if site is CE, EE Code will have to manually be install
-diff $HOME/magento-core-files/app/code/core/ $HOME/current/app/code/core/
+diff $FROMPATH/app/code/core/ $TOPATH/app/code/core/
 if [ $? -ne 0 ]
 then
-      diff -wrql $HOME/magento-core-files/app/code/core/ $HOME/current/app/code/core/ >> $HOME/core-diff.log
-      diff -wrql $HOME/magento-core-files/app/code/core/ $HOME/current/app/code/core/ | mutt -s "Core File Changes" -- $EMAILTO
+      diff -wrql $FROMPATH/app/code/core/ $TOPATH/app/code/core/ >> $HOME/core-diff.log
+      diff -wrql $FROMPATH/app/code/core/ $TOPATH/app/code/core/ | mutt -s "Core File Changes" -- $EMAILTO
       #TODO copy and commit files to repo and push to git so files can be quickly reviewed
 else
     echo "Core files are the same" >> $HOME/core-diff.log
 fi
 # Add Core Lib checker
-diff $HOME/magento-core-files/lib/ $HOME/current/lib/
+diff $FROMPATH/lib/ $TOPATH/lib/
 if [ $? -ne 0 ]
 then
-      diff -wrql $HOME/magento-core-files/lib/ $HOME/current/lib/ >> $HOME/core-diff.log
-      diff -wrql $HOME/magento-core-files/lib/ $HOME/current/lib/ | mutt -s "Core File Changes" -- $EMAILTO
+      diff -wrql $FROMPATH/lib/ $TOPATH/lib/ >> $HOME/core-diff.log
+      diff -wrql $FROMPATH/lib/ $TOPATH/lib/ | mutt -s "Core File Changes" -- $EMAILTO
 else
     echo "lib files are the same" >> $HOME/core-diff.log
 fi

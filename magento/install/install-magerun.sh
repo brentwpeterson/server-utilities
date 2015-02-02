2f205c7e7973d12bc085a3be0614015c88b68199
Brent W. Peterson <creativedata@gmail.com>
2015-02-01 18:20:59 -0600

    Update install-magerun.sh

--- magento/install/install-magerun.sh
+++ magento/install/install-magerun.sh
@@ -3,6 +3,7 @@ wget https://raw.githubusercontent.com/netz98/n98-magerun/master/n98-magerun.pha
 chmod +x ./n98-magerun.phar
 sudo cp ./n98-magerun.phar /usr/local/bin/
 mkdir -p ~/.n98-magerun/modules/
-#Add Fabrizio's Corehacks add on
+# Add Fabrizio's Corehacks add on
+# https://github.com/AOEpeople/mpmd
 git clone https://github.com/AOEpeople/mpmd.git ~/.n98-magerun/modules/mpmd
 sed -i '2ialias magerun="~/./n98-magerun.phar"' ~/.bashrc

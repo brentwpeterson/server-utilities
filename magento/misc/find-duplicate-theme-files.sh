ddf1d4dbc67d2248ca1daa2ddf1307b81a0c01eb
Brent W. Peterson <creativedata@gmail.com>
2015-02-12 12:58:41 -0600

    Update find-duplicate-theme-files.sh

--- magento/misc/find-duplicate-theme-files.sh
+++ magento/misc/find-duplicate-theme-files.sh
@@ -1,4 +1,10 @@
 #This is only a start
+# #########################
+# Brent Peterson support@wagento.com
+# ** WARNING ** DO NOT USE THIS ON PRODUCTION
+# DO NOT USE THIS UNLESS YOU HAVE TESTED IT ON YOUR LOCAL SYSTEM
+# DO NOT USE THIS IF YOU HAVE NO IDEA WHAT YOU ARE DOING
+#########################
 #TODO EVERYTHING
 fileclean='clean/path'
 filecustom='custom/path'

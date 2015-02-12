eae37acefed83656c8142ed967895f8284ca6df2
Brent W. Peterson <creativedata@gmail.com>
2015-02-12 13:00:07 -0600

    Update get-stats-for-Magento.sh

--- magento/misc/get-stats-for-Magento.sh
+++ magento/misc/get-stats-for-Magento.sh
@@ -1,3 +1,10 @@
+# #########################
+# Brent Peterson support@wagento.com
+# ** WARNING ** DO NOT USE THIS ON PRODUCTION
+# DO NOT USE THIS UNLESS YOU HAVE TESTED IT ON YOUR LOCAL SYSTEM
+# DO NOT USE THIS IF YOU HAVE NO IDEA WHAT YOU ARE DOING
+#########################
+
 FILE=$1
 
 ## MySQL is not always on the same server

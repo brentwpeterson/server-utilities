f7796b606a9740cc366347da5b248a21ba82863d
Brent W. Peterson <creativedata@gmail.com>
2015-02-12 13:03:23 -0600

    Update remove-old-db-backups.sh

--- utilities/files/remove-old-db-backups.sh
+++ utilities/files/remove-old-db-backups.sh
@@ -1,2 +1,9 @@
 ####This is just a start
+# #########################
+# Brent Peterson support@wagento.com
+# ** WARNING ** DO NOT USE THIS ON PRODUCTION
+# DO NOT USE THIS UNLESS YOU HAVE TESTED IT ON YOUR LOCAL SYSTEM
+# DO NOT USE THIS IF YOU HAVE NO IDEA WHAT YOU ARE DOING
+# It would probably be better that no one uses this one
+#########################
 find $home/path/to/files/ -mtime +30 -name '*.gz' -exec rm {} \;

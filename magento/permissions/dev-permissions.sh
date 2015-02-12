7dfa2de34b546aa00b323e796cff86a5f49ea59e
Brent W. Peterson <creativedata@gmail.com>
2015-02-12 09:57:55 -0600

    Update dev-permissions.sh
    Add check on input

--- magento/permissions/dev-permissions.sh
+++ magento/permissions/dev-permissions.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 #########################
+# This requires a value input. 
 # Ask for the app location
 # this is from http://www.magentocommerce.com/knowledge-base/entry/install-privs-after
 #########################
+
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 1
+fi
 
 find $1/. -type f -exec chmod 600 {} \;
 find $1/. -type d -exec chmod 700 {} \; 

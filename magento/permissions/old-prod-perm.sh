5cf95f07d1949dedafe387c6cf6a3a8f588b39e5
Brent W. Peterson <creativedata@gmail.com>
2015-02-12 09:59:19 -0600

    Update old-prod-perm.sh

--- magento/permissions/old-prod-perm.sh
+++ magento/permissions/old-prod-perm.sh
@@ -5,6 +5,12 @@
 #########################
 ##
 # Take first positional parameter as Magento base directory
+
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 1
+fi
+
 path="$1"
 
 ##

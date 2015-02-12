cf5a981cb163457f84ab1717e45e1b0c539157dc
Brent W. Peterson <creativedata@gmail.com>
2015-02-12 12:55:16 -0600

    Update dev-permissions.sh

--- magento/permissions/dev-permissions.sh
+++ magento/permissions/dev-permissions.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #########################
+# Brent Peterson support@wagento.com
+# ** WARNING ** DO NOT USE THIS ON PRODUCTION
+# DO NOT USE THIS UNLESS YOU HAVE TESTED IT ON YOUR LOCAL SYSTEM
+# DO NOT USE THIS IF YOU HAVE NO IDEA WHAT YOU ARE DOING
 # This requires a value input. 
 # Ask for the app location
 # this is from http://www.magentocommerce.com/knowledge-base/entry/install-privs-after

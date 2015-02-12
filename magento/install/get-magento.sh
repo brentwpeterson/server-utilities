399e96f6a1376d0a83055360fbb317c6a8c20af2
Brent W. Peterson <creativedata@gmail.com>
2015-02-12 13:02:09 -0600

    Update get-magento.sh

--- magento/install/get-magento.sh
+++ magento/install/get-magento.sh
@@ -1,3 +1,9 @@
+# #########################
+# Brent Peterson support@wagento.com
+# ** WARNING ** DO NOT USE THIS ON PRODUCTION
+# DO NOT USE THIS UNLESS YOU HAVE TESTED IT ON YOUR LOCAL SYSTEM
+# DO NOT USE THIS IF YOU HAVE NO IDEA WHAT YOU ARE DOING
+#########################
 echo "Enter Magento Version to download, (Default is 1.9.1.0)followed by [ENTER]:"
 read version
 echo "Do you want to download from a different locaion? The default is ~/ followed by [ENTER]:"

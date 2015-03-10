04a36df6af57e2eb9aff4d78ad19acbc7f459212
Brent Peterson <brent@wagento.com>
2015-03-09 21:15:01 -0500

    Update git branch removal to include remote

--- utilities/git/remove-merged-branches.sh
+++ utilities/git/remove-merged-branches.sh
@@ -6,4 +6,19 @@ for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master`; do
 done
 
 #Delete all local branches that have already been merged to master
-git branch --merged master | grep -v "\* master" | xargs -n 1 git branch -d
+#git branch --merged master | grep -v "\* master" | xargs -n 1 git branch -d
+
+merged_branches(){
+local current_branch=$(git rev-parse --abbrev-ref HEAD)
+for branch in $(git branch --merged | cut -c3-)
+  do
+  echo "Branch $branch is already merged into $current_branch."
+#  echo "Would you like to delete it? [Y]es/[N]o "
+#  read REPLY
+#  if [[ $REPLY =~ ^[Yy] ]]; then
+       git branch -d $branch
+       git push origin :$branch
+#  fi
+   done
+}
+merged_branches

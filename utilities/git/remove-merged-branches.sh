c2ed643579ed7fc15caafe63e7a6e2420b2155d1
Brent Peterson <brent@wagento.com>
2015-03-16 15:37:34 -0500

    update remove files

--- utilities/git/remove-merged-branches.sh
+++ utilities/git/remove-merged-branches.sh
@@ -13,10 +13,15 @@ local current_branch=$(git rev-parse --abbrev-ref HEAD)
 for branch in $(git branch --merged | cut -c3-)
   do
   echo "Branch $branch is already merged into $current_branch."
-#TODO Make sure it is not master
-       git branch -d $branch
-       git push origin :$branch
-
+#  echo "Would you like to delete it? [Y]es/[N]o "
+#  read REPLY
+#  if [[ $REPLY =~ ^[Yy] ]]; then
+  if [[ $branch -ne 'master' ]]; then
+    git branch -d $branch
+#       git push origin :$branch
+   else
+	   echo 'This is the master branch'
+   fi
    done
 }
 merged_branches

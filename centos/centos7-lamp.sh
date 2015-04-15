9d589cea8f1238bba018b219d205122cbad809f3
Brent W. Peterson <creativedata@gmail.com>
2015-04-15 14:21:28 -0500

    Update centos7-lamp.sh

--- centos/centos7-lamp.sh
+++ centos/centos7-lamp.sh
@@ -22,20 +22,6 @@ read -p "Install Firewall? [yn]" answer
 if [ $answer == y ]; then
 yum install system-config-firewall
 fi
-#hostname
-#read hostn
-#ip='127.0.0.1 '
-#addhost = ${ip}${hostn}
-#echo $addhost >> /etc/hosts
 service httpd restart; service mysqld restart
 yum update
 service httpd restart; service mysqld restart
-useradd wagento
-echo 'wagento         ALL=(ALL)       NOPASSWD: ALL' >> /etc/sudoers
-su wagento
-cd ~/ 
-wget https://raw.githubusercontent.com/netz98/n98-magerun/master/n98-magerun.phar
-chmod +x ./n98-magerun.phar
-sudo cp ./n98-magerun.phar /usr/local/bin/
-sed -i '2ialias magerun="~/./n98-magerun.phar"' ~/.bashrc
-exit

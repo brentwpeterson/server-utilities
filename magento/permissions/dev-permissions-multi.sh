4de0280330df0b0d6c2b121b4f7dd0f4d36ae0b5
Brent Peterson <brent@wagento.com>
2015-04-28 23:28:11 +0000

    add dev multi server permissions

---
+++ magento/permissions/dev-permissions-multi.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#########################
+# Brent Peterson support@wagento.com
+# ** WARNING ** DO NOT USE THIS ON PRODUCTION
+# DO NOT USE THIS UNLESS YOU HAVE TESTED IT ON YOUR LOCAL SYSTEM
+# DO NOT USE THIS IF YOU HAVE NO IDEA WHAT YOU ARE DOING
+# This requires a value input. 
+# Ask for the app location
+# this is from http://www.magentocommerce.com/knowledge-base/entry/install-privs-after
+#########################
+
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 1
+fi
+
+find $1/. -type f -exec chmod 660 {} \;
+find $1/. -type d -exec chmod 770 {} \; 
+find $1/.git/ -type d -exec chmod 770 {} \; 
+find $1/.git/ -type f -exec chmod 660 {} \; 
+chmod 660 $1/.gitignore 

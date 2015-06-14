d11a12ba4858df9de0785cc96aa47e3b98191ff6
Brent W. Peterson <creativedata@gmail.com>
2015-06-14 13:12:33 -0500

    Make default download 1.9.1.1

--- magento/install/get-magento.sh
+++ magento/install/get-magento.sh
@@ -4,12 +4,12 @@
 # DO NOT USE THIS UNLESS YOU HAVE TESTED IT ON YOUR LOCAL SYSTEM
 # DO NOT USE THIS IF YOU HAVE NO IDEA WHAT YOU ARE DOING
 #########################
-echo "Enter Magento Version to download, (Default is 1.9.1.0)followed by [ENTER]:"
+echo "Enter Magento Version to download, (Default is 1.9.1.1)followed by [ENTER]:"
 read version
 echo "Do you want to download from a different locaion? The default is ~/ followed by [ENTER]:"
 read backdir
 backdir=${backdir:-$HOME}
-version=${version:-1.9.1.0}
+version=${version:-1.9.1.1}
 wget http://www.magentocommerce.com/downloads/assets/$version/magento-$version.tar.gz -P $backdir
 #####
 # TODO : Add this as an option for Sample data

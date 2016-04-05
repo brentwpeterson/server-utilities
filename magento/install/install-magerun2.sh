3cad1e9ecd1b6f1415a59ebf99fc8ab3bbec7320
Brent Peterson <creativedata@gmail.com>
2016-04-05 09:32:14 -0400

    Update with Magento2 Magerun installer

---
+++ magento/install/install-magerun2.sh
@@ -0,0 +1,14 @@
+# #########################
+# Brent Peterson support@wagento.com
+# ** WARNING ** DO NOT USE THIS ON PRODUCTION
+# DO NOT USE THIS UNLESS YOU HAVE TESTED IT ON YOUR LOCAL SYSTEM
+# DO NOT USE THIS IF YOU HAVE NO IDEA WHAT YOU ARE DOING
+#########################
+cd ~/
+wget https://files.magerun.net/n98-magerun2.phar
+chmod +x ./n98-magerun2.phar
+cp ./n98-magerun2.phar /usr/local/bin/
+sed -i '2ialias magerun2="~/./n98-magerun2.phar"' ~/.bashrc
+
+# source .bashrc to your current terminal instance
+source  ~/.bashrc

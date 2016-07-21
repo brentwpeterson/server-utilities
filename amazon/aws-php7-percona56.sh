dc2e8e8f1e2bc0a6434ffd9380675b805aa51eec
Brent W. Peterson <creativedata@gmail.com>
2016-07-21 13:36:56 -0500

    optional apache install
    Make Apache optional

--- amazon/aws-php7-percona56.sh
+++ amazon/aws-php7-percona56.sh
@@ -5,7 +5,17 @@
 yum groupinstall 'Development Tools'
 yum install http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm
 yum install Percona-Server-client-56 Percona-Server-server-56
+#Install apache
 #yum remove httpd httpd-tools
+#yum -y install ntp httpd24 mod24_ssl httpd24-tools httpd24-devel
+#yum -y install mod24_fcgid 
+#chkconfig --levels 235 httpd on
+#/etc/init.d/httpd start
+#mv /etc/httpd/conf.d/fcgid.conf /etc/httpd/conf.d/fcgid.bak
+#mkdir -p /etc/httpd/vhosts.d
+#sed -i '95iServerName localhost' /etc/httpd/conf/httpd.conf
+#echo 'IncludeOptional vhosts.d/*.conf' >> /etc/httpd/conf/httpd.conf
+
 chkconfig --levels 235 mysql on
 /etc/init.d/mysql start
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
@@ -13,21 +23,10 @@ rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
 #install PHP
 yum -y install php70w php70w-devel php70w-gd php70w-imap php70w-ldap php70w-mysql php70w-odbc php70w-pear php70w-xml php70w-xmlrpc php70w-pecl-apc php70w-mbstring php70w-mcrypt php70w-snmp php70w-soap php70w-tidy 
 yum -y install curl curl-devel perl-libwww-perl ImageMagick libxml2 libxml2-devel php70w-cli mutt
-#Install apache
-yum remove httpd httpd-tools
-yum -y install ntp httpd24 mod24_ssl httpd24-tools httpd24-devel
-#yum -y install mod24_fcgid 
 
-mv /etc/httpd/conf.d/fcgid.conf /etc/httpd/conf.d/fcgid.bak
-mkdir -p /etc/httpd/vhosts.d
-mkdir -p /var/www/vhosts/
-sed -i '95iServerName localhost' /etc/httpd/conf/httpd.conf
-echo 'IncludeOptional vhosts.d/*.conf' >> /etc/httpd/conf/httpd.conf
-chkconfig --levels 235 httpd on
-/etc/init.d/httpd start
-service httpd restart; service mysql restart
+service mysql restart
 yum update
-service httpd restart; service mysql restart
+
 cd /home/ec2-user/
 wget https://raw.githubusercontent.com/netz98/n98-magerun/master/n98-magerun.phar
 chmod +x ./n98-magerun.phar
@@ -37,6 +36,7 @@ sed -i '2ialias magerun="~/./n98-magerun.phar"' /home/ec2-user/.bashrc
 # http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/install-LAMP.html
 # TODO need to add/update the amazon permissions
 ################### 
+mkdir -p /var/www/vhosts/
 chmod 2775 /var/www
 find /var/www -type d -exec sudo chmod 2775 {} +
 find /var/www -type f -exec sudo chmod 0664 {} +

e6dfe8d41642f5d63ac87eda1ad099075508438e
Brent W. Peterson <creativedata@gmail.com>
2016-08-24 15:00:31 -0500

    Update aws-php70w-apache.sh
    Update amazon repo exlusion

--- amazon/aws-php70w-apache.sh
+++ amazon/aws-php70w-apache.sh
@@ -2,15 +2,17 @@
 # TODO 
 #########
 yum groupinstall 'Development Tools'
-#yum -y install ntp httpd24 mod24_ssl httpd24-tools httpd24-devel
-#yum -y install mod24_fcgid 
-#chkconfig --levels 235 httpd on
-#/etc/init.d/httpd start
-#mv /etc/httpd/conf.d/fcgid.conf /etc/httpd/conf.d/fcgid.bak
-#sed -i '95iServerName localhost' /etc/httpd/conf/httpd.conf
-#echo 'IncludeOptional vhosts.d/*.conf' >> /etc/httpd/conf/httpd.conf
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
 rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
+
+sed -i '15iexclude=httpd-tools* httpd*' /etc/yum.repos.d/amzn-main.repo 
+yum -y install ntp httpd24 mod24_ssl httpd24-tools httpd24-devel
+yum -y install mod24_fcgid 
+chkconfig --levels 235 httpd on
+/etc/init.d/httpd start
+mv /etc/httpd/conf.d/fcgid.conf /etc/httpd/conf.d/fcgid.bak
+sed -i '95iServerName localhost' /etc/httpd/conf/httpd.conf
+echo 'IncludeOptional vhosts.d/*.conf' >> /etc/httpd/conf/httpd.conf
 
 #install PHP
 yum -y install php70w php70w-devel php70w-gd php70w-imap php70w-fpm php70w-ldap php70w-mysql php70w-pear php70w-xml php70w-xmlrpc php70w-curl php70w-mbstring php70w-mcrypt php70w-snmp php70w-soap php70w-tidy 

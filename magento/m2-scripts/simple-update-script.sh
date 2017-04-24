8434564382505866a2972a8325af0c0db5dc27a2
Brent Peterson <brent@wagento.com>
2017-04-24 13:23:13 -0400

    comment file

--- magento/m2-scripts/simple-update-script.sh
+++ magento/m2-scripts/simple-update-script.sh
@@ -1,7 +1,13 @@
+#replace with the from and to versions
 sed -ie 's/2.1.5/2.1.6/g' composer.json 
+#run composer update
 composer update
+#comfirm that magento is executable
 chmod u+x bin/magento
+#run magento setup
 ./bin/magento set:up
+#clear cache
 ./bin/magento cache:clean
 ./bin/magento cache:flush
+./bin/magento --version
 

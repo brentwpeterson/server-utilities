c1aeb7e52a5a4402e69becf16213cd6650a0186b
Brent W. Peterson <creativedata@gmail.com>
2023-04-26 14:38:09 -0500

    Upadte to PHP 8.1

--- ubuntu/server-setup-magento2.sh
+++ ubuntu/server-setup-magento2.sh
@@ -1,84 +1,69 @@
 #!/bin/bash
+set -e
 
-# Update system
-apt-get update
-apt-get upgrade -y
+# Ensure the script is run with root privileges
+if [ "$(id -u)" -ne 0 ]; then
+  echo "This script must be run as root" >&2
+  exit 1
+fi
 
-# Install necessary tools
-apt-get install -y software-properties-common wget curl unzip
+# Update package index
+apt update
 
-# Add repository for PHP 8
-add-apt-repository ppa:ondrej/php -y
-apt-get update
+# Install required packages
+apt install -y software-properties-common wget
 
-# Install NGINX, PHP 8 and required PHP extensions for Magento 2.4
-apt-get install -y nginx php8.0 php8.0-fpm php8.0-mysql php8.0-dom php8.0-simplexml php8.0-xml php8.0-curl php8.0-xsl php8.0-gd php8.0-xmlreader php8.0-mbstring php8.0-intl php8.0-iconv php8.0-zip php8.0-soap php8.0-sockets php8.0-bcmath
+# Add repository for PHP 8.1
+add-apt-repository -y ppa:ondrej/php
+apt update
+
+# Install PHP 8.1 and required extensions
+apt install -y php8.1 php8.1-fpm php8.1-mysql php8.1-dom php8.1-simplexml php8.1-soap php8.1-gd php8.1-iconv php8.1-mbstring php8.1-ctype php8.1-zip php8.1-pdo php8.1-xml php8.1-bcmath php8.1-json php8.1-intl php8.1-curl php8.1-xsl
+
+# Install NGINX
+apt install -y nginx
 
 # Install MariaDB
-apt-get install -y mariadb-server mariadb-client
-
-# Start and enable services
-systemctl start nginx
-systemctl enable nginx
-systemctl start php8.0-fpm
-systemctl enable php8.0-fpm
-systemctl start mariadb
-systemctl enable mariadb
+apt install -y mariadb-server
 
 # Configure MariaDB
 mysql_secure_installation
 
-# Create Magento database and user
-mysql -u root -p -e "CREATE DATABASE magento_db; CREATE USER 'magento_user'@'localhost' IDENTIFIED BY 'your_magento_user_password'; GRANT ALL PRIVILEGES ON magento_db.* TO 'magento_user'@'localhost'; FLUSH PRIVILEGES;"
-
 # Install Composer
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
-chmod +x /usr/local/bin/composer
+apt install -y composer
 
-# Download Magento 2.4
-wget https://github.com/magento/magento2/archive/2.4.zip
-unzip 2.4.zip
-mv magento2-2.4 /var/www/html/magento
-
-# Set permissions for Magento
-chown -R www-data:www-data /var/www/html/magento
-chmod -R 755 /var/www/html/magento
-
-# Create NGINX site configuration for Magento
-cat > /etc/nginx/sites-available/magento << EOL
+# Create NGINX server block file for Magento
+cat > /etc/nginx/sites-available/magento <<EOL
 upstream fastcgi_backend {
-    server unix:/run/php/php8.0-fpm.sock;
+  server  unix:/run/php/php8.1-fpm.sock;
 }
 
 server {
-    listen 80;
-    server_name yourdomain.com;
-    set \$MAGE_ROOT /var/www/html/magento;
-    set \$MAGE_MODE developer;
-    include /var/www/html/magento/nginx.conf.sample;
+  listen 80;
+  server_name example.com;
+  set \$MAGE_ROOT /var/www/html/magento;
+  include /var/www/html/magento/nginx.conf.sample;
 }
 EOL
 
-# Enable the Magento site
+# Enable the NGINX server block
 ln -s /etc/nginx/sites-available/magento /etc/nginx/sites-enabled/
+
+# Disable the default NGINX server block
 rm /etc/nginx/sites-enabled/default
+
+# Restart NGINX and PHP-FPM services
 systemctl restart nginx
+systemctl restart php8.1-fpm
 
-# Install Magento 2.4 using Composer
-cd /var/www/html/magento
-composer install
+# Download Magento 2.4
+cd /var/www/html
+composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition magento
 
-# Run Magento installation
-bin/magento setup:install --base-url=http://yourdomain.com/ \
---db-host=localhost \
---db-name=magento_db \
---db-user=magento_user \
---db-password=your_magento_user_password \
---admin-firstname=Admin \
---admin-lastname=User \
---admin-email=admin@example.com \
---admin-user=admin \
---admin-password=admin123 \
---language=en_US \
---currency=USD
+# Set proper permissions for Magento
+chown -R www-data:www-data /var/www/html/magento
+find /var/www/html/magento -type d -exec chmod 770 {} \;
+find /var/www/html/magento -type f -exec chmod 660 {} \;
+chmod +x /var/www/html/magento/bin/magento
+
+echo "Magento installation completed. Please proceed with the browser-based installation."
